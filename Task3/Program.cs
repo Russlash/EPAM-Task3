d975b0958a0ddd2b9d1eab73ae5f8c7a285a9cff
Руслан Гусейнов <guseinov.ruslan9@mail.ru>
2019-09-22 17:49:53 +0400

    Добавьте файлы проекта.

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Привет, привет мир Май! май труд ".ToLower();
+            var result = str.Split(new[] { ' ', '.', ',', '?', '!', ':', ';' }, StringSplitOptions.RemoveEmptyEntries)
+                .GroupBy(x => x)
+                .Where(x => x.Count() == 1)
+                .Select(x => x.Key);
+            foreach (var item in result) Console.Write(item + " ");
+            Console.ReadLine();
+        }
+    }
+}
